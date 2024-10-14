84bb3d41db083768096aedba468f95f719cbcdc5
bizzy-coding <bizzycoding@gmail.com>
2024-10-14 11:55:58 +0100

    NAV - adding in the icons for additional navigation actions

--- src/components/common/Navigation.jsx
+++ src/components/common/Navigation.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import logo from '../../assets/logo.svg';
+import cartIcon from '../../assets/cartIcon.svg';
+import accountIcon from '../../assets/accountIcon.svg';
+import searchIcon from '../../assets/searchIcon.svg';
+import wishIcon from '../../assets/wishIcon.svg';
 
 function Navigation() {
     return(
         <>
         <nav>
-            <div className="nav-icon-wrap"></div>
+            <div className="nav-icon-wrap">
+                <img src={cartIcon} alt="" />
+                <img src={wishIcon} alt="" />
+
+            </div>
             <div className="logo">
             <img src={logo} alt="" width="100px"/>
 
             </div>
-            <div className="nav-icon-wrap"></div>
+            <div className="nav-icon-wrap">
+            <img src={accountIcon} alt="" />
+                <img src={searchIcon} alt="" />
+            </div>
         </nav>
         </>
     )
